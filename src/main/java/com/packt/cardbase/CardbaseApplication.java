55cf8a1ab1c29758f4b3c993ccdb9ca45f836bee
mohamedfazilk <2114921@cognizant.com>
2022-09-12 15:30:11 +0530

    first spring boot project

---
+++ src/main/java/com/packt/cardbase/CardbaseApplication.java
@@ -0,0 +1,13 @@
+package com.packt.cardbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardbaseApplication.class, args);
+	}
+
+}
