6cdf4753b4ab3ff6469cdf67a4f9940ac7a7a3e5
mohamedfazilk <2114921@cognizant.com>
2022-09-13 22:32:23 +0530

    place @entity,@id into the car class

--- src/main/java/com/packt/cardbase/domain/Car.java
+++ src/main/java/com/packt/cardbase/domain/Car.java
@@ -1,5 +1,16 @@
 package com.packt.cardbase.domain;
 
-public class Car {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class Car {
+	 @Id
+	 @GeneratedValue(strategy=GenerationType.AUTO)
+	 
+	private long id;
+	private String brand, model, color,
+	 registerNumber;
+	private int year, price;
 }
