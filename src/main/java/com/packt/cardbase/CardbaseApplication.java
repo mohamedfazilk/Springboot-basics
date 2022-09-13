6db9f81a4a44b6af0932b996ddcaa7a08a1b090e
mohamedfazilk <2114921@cognizant.com>
2022-09-13 21:50:48 +0530

    Adding Log into the code and print a msg as info

--- src/main/java/com/packt/cardbase/CardbaseApplication.java
+++ src/main/java/com/packt/cardbase/CardbaseApplication.java
@@ -1,13 +1,20 @@
 package com.packt.cardbase;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CardbaseApplication {
+	
+	private static final Logger logger =LoggerFactory.getLogger
+			(CardbaseApplication.class);
+	
 
 	public static void main(String[] args) {
+		 
 		SpringApplication.run(CardbaseApplication.class, args);
+		logger.info("Application started now");
 	}
 
 }
