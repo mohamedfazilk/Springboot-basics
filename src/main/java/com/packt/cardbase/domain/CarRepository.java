0f2ff57d7618b6acc9305f9151386e7dedf788ec
mohamedfazilk <2114921@cognizant.com>
2022-09-17 16:05:20 +0530

    List all the data by its brand

--- src/main/java/com/packt/cardbase/domain/CarRepository.java
+++ src/main/java/com/packt/cardbase/domain/CarRepository.java
@@ -1,7 +1,11 @@
 package com.packt.cardbase.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface CarRepository  extends CrudRepository<Car, Long>{
+	
+	List<Car> findByBrand(String brand);
 
 }
