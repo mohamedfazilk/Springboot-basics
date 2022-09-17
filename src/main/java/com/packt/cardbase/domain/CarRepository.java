50c1eeba5f087837224c50335e3a17f2ea5bf704
mohamedfazilk <2114921@cognizant.com>
2022-09-17 20:31:03 +0530

    place @Query annotaion

--- src/main/java/com/packt/cardbase/domain/CarRepository.java
+++ src/main/java/com/packt/cardbase/domain/CarRepository.java
@@ -2,10 +2,13 @@ package com.packt.cardbase.domain;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CarRepository  extends CrudRepository<Car, Long>{
 	
+	
+	@Query("select c from Car c where c.brand=?1")
 	List<Car> findByBrand(String brand);
 
 }
