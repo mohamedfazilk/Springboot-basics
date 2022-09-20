eaca5563bac95093a968894e44ab69eb72cb22bf
mohamedfazilk <2114921@cognizant.com>
2022-09-20 23:34:04 +0530

    remove the  userDetailsService() method from the SecurityConfig class to disable

--- src/main/java/com/packt/cardbase/SecurityConfig.java
+++ src/main/java/com/packt/cardbase/SecurityConfig.java
@@ -1,31 +1,38 @@
 package com.packt.cardbase;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class SecurityConfig  {
-	@Bean
-	 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        
-        return http.build();
-    }
-	
-	public UserDetailsService userDetailsService() {
-		UserDetails user =User.withDefaultPasswordEncoder()
-				 .username("user")
-				 .password("password")
-				 .roles("USER")
-				 .build();
+import com.packt.cardbase.service.UserDetailsServiceImpl;
 
-				 return new InMemoryUserDetailsManager(user);
-				}
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+//	public UserDetailsService userDetailsService() {
+//		UserDetails user =User.withDefaultPasswordEncoder()
+//				 .username("user")
+//				 .password("password")
+//				 .roles("USER")
+//				 .build();
+//
+//				 return new InMemoryUserDetailsManager(user);
+//				}
 }
