0e8063221a8d6958ea21405dfc6814b028a55931
mohamedfazilk <2114921@cognizant.com>
2022-09-20 21:54:36 +0530

    adding spring security dependencies

--- src/main/java/com/packt/cardbase/domain/CarRepository.java
+++ src/main/java/com/packt/cardbase/domain/CarRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
-@RepositoryRestResource(path="vehicles")
+
 public interface CarRepository  extends CrudRepository<Car, Long>{
 	
 	
