051c92c6c3803138db050da44935ff454bc46126
mohamedfazilk <2114921@cognizant.com>
2022-09-20 23:25:41 +0530

    UserDetailsImpl implements the UserDedtailsService and  place the UserDetails

--- src/main/java/com/packt/cardbase/service/UserDetailsServiceImpl.java
+++ src/main/java/com/packt/cardbase/service/UserDetailsServiceImpl.java
@@ -1,5 +1,39 @@
 package com.packt.cardbase.service;
 
-public class UserDetailsServiceImpl {
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.packt.cardbase.domain.User;
+import com.packt.cardbase.domain.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	@Autowired
+	private UserRepository userrepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<User> user = userrepository.findByUsername(username);
+		UserBuilder builder = null;
+
+		if (user.isPresent()) {
+			User currentUser = user.get();
+			builder = org.springframework.security.core.userdetails.User.withUsername(username);
+			builder.password(currentUser.getPassword());
+			builder.roles(currentUser.getRole());
+		}
+
+		else {
+			throw new UsernameNotFoundException("User notfound.");
+		}
+		return builder.build();
+
+	}
 
 }
