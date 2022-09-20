3a5331319d017f8b593fb95d186d21b3bd0bcc05
mohamedfazilk <2114921@cognizant.com>
2022-09-20 22:54:25 +0530

    create a  new package and UserDetailsServiceImpl class

--- src/main/java/com/packt/cardbase/domain/UserRepository.java
+++ src/main/java/com/packt/cardbase/domain/UserRepository.java
@@ -1,7 +1,11 @@
 package com.packt.cardbase.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+	
+	Optional<User>findByUsername(String username);
 
 }
