0b396d155e8d26d0377455ce4882e21fb6eeb3ec
mohamedfazilk <2114921@cognizant.com>
2022-09-22 14:49:51 +0530

    Generate the secret key andsigned JWT token

--- src/main/java/com/packt/cardbase/service/JwtService.java
+++ src/main/java/com/packt/cardbase/service/JwtService.java
@@ -1,22 +1,30 @@
 package com.packt.cardbase.service;
 
 import java.security.Key;
+import java.util.Date;
 
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-
 
 @Component
 public class JwtService {
 	// 1 day in ms
 	static final long EXPIRATIONTIME = 8640000;
-	
+
 	static final String PREFIX = "Bearer";
-	
-	
-	 // Generate secret key. Only for the demonstration
+
+	// Generate secret key. Only for the demonstration
 	static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+	 // Generate signed JWT token
+	public String getToken(String username) {
+		String token = Jwts.builder().setSubject(username)
+				.setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
+				.signWith(key).compact();
+		return token;
+	}
 
 }
