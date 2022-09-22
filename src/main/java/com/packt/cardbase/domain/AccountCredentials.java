9cd1c33a9f79589d4c9ba638b2fa25b461a43b50
mohamedfazilk <2114921@cognizant.com>
2022-09-22 15:00:29 +0530

    Get a token from request Authorization header and verify a token and get username,

---
+++ src/main/java/com/packt/cardbase/domain/AccountCredentials.java
@@ -0,0 +1,5 @@
+package com.packt.cardbase.domain;
+
+public class AccountCredentials {
+
+}
--- src/main/java/com/packt/cardbase/service/JwtService.java
+++ src/main/java/com/packt/cardbase/service/JwtService.java
@@ -3,6 +3,9 @@ package com.packt.cardbase.service;
 import java.security.Key;
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwts;
@@ -19,12 +22,29 @@ public class JwtService {
 	// Generate secret key. Only for the demonstration
 	static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
-	 // Generate signed JWT token
+	// Generate signed JWT token
 	public String getToken(String username) {
 		String token = Jwts.builder().setSubject(username)
-				.setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
-				.signWith(key).compact();
+				.setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME)).signWith(key).compact();
 		return token;
+	}
+
+	// Get a token from request Authorization header,
+	// verify a token and get username
+
+	public String getAuthUser(HttpServletRequest request) {
+		String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+		if (token != null) {
+			String user = Jwts.parserBuilder().setSigningKey(key)
+					.build().
+					parseClaimsJws(token.replace(PREFIX, ""))
+					.getBody().getSubject();
+			if (user != null)
+				return user;
+		}
+		return null;
+
 	}
 
 }
