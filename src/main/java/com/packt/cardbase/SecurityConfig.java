0a4a25f43f52e4b88acba9feb60a0706e51233db
mohamedfazilk <2114921@cognizant.com>
2022-09-22 15:50:26 +0530

    injected AuthenticationManager into the SecurityConfig

--- src/main/java/com/packt/cardbase/SecurityConfig.java
+++ src/main/java/com/packt/cardbase/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.packt.cardbase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,4 +36,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //
 //				 return new InMemoryUserDetailsManager(user);
 //				}
+	@Bean
+	public AuthenticationManager getAuthenticationManager() throws Exception{
+		return authenticationManager();
+		
+	}
 }
