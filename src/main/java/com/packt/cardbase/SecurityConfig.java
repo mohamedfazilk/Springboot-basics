2623956e1fb11c0ee2ac9e171c7f6b10937278cf
mohamedfazilk <2114921@cognizant.com>
2022-09-22 17:52:25 +0530

    place the functionality for the diable csrf and create POST request to thge  endpoint

--- src/main/java/com/packt/cardbase/SecurityConfig.java
+++ src/main/java/com/packt/cardbase/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.packt.cardbase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -27,6 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 	}
+
 //	public UserDetailsService userDetailsService() {
 //		UserDetails user =User.withDefaultPasswordEncoder()
 //				 .username("user")
@@ -40,5 +43,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public AuthenticationManager getAuthenticationManager() throws Exception{
 		return authenticationManager();
 		
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.
+				STATELESS). and().authorizeHttpRequests()
+		// POST request to /login endpoint is not secured
+		.antMatchers(HttpMethod.POST, "/login")
+		.permitAll()
+		// All other requests are secured
+		.anyRequest().authenticated();
 	}
 }
