52a7444d54a3c4ad6460dbbff872dc51b35f6355
mohamedfazilk <2114921@cognizant.com>
2022-09-23 15:25:38 +0530

    cross-origin, it adds proper headers to the request

--- src/main/java/com/packt/cardbase/SecurityConfig.java
+++ src/main/java/com/packt/cardbase/SecurityConfig.java
@@ -17,20 +17,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.packt.cardbase.service.UserDetailsServiceImpl;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsServiceImpl userDetailsService;
-	
+
 	@Autowired
 	private AuthenticationFilter authenticationFilter;
-	
+
 	@Autowired
-	private AuthEntryPoint  exceptionHandler;
+	private AuthEntryPoint exceptionHandler;
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -47,23 +51,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //				 return new InMemoryUserDetailsManager(user);
 //				}
 	@Bean
-	public AuthenticationManager getAuthenticationManager() throws Exception{
+	public AuthenticationManager getAuthenticationManager() throws Exception {
 		return authenticationManager();
-		
+
+	}
+
+	// Add Global CORS filter inside the class
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(Arrays.asList("*"));
+		config.setAllowedMethods(Arrays.asList("*"));
+		config.setAllowedHeaders(Arrays.asList("*"));
+		config.setAllowCredentials(false);
+		config.applyPermitDefaultValues();
+
+		source.registerCorsConfiguration("/**", config);
+		return source;
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().sessionManagement()
-		.sessionCreationPolicy(SessionCreationPolicy.
-				STATELESS). and().authorizeHttpRequests()
-		// POST request to /login endpoint is not secured
-		.antMatchers(HttpMethod.POST, "/login")
-		.permitAll()
-		// All other requests are secured
-		.anyRequest().authenticated().and()
-		 .exceptionHandling()
-		 .authenticationEntryPoint(exceptionHandler).and()
-		.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		http.csrf().disable().cors().and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.authorizeHttpRequests()
+				// POST request to /login endpoint is not secured
+				.antMatchers(HttpMethod.POST, "/login").permitAll()
+				// All other requests are secured
+				.anyRequest().authenticated().and().exceptionHandling().authenticationEntryPoint(exceptionHandler).and()
+				.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 }
