197f36664512a42bb28690e8c4cd07292f689953
mohamedfazilk <2114921@cognizant.com>
2022-09-23 16:01:06 +0530

    add Optional calss and write the query for  find the first name

--- src/main/java/com/packt/cardbase/domain/OwnerRepository.java
+++ src/main/java/com/packt/cardbase/domain/OwnerRepository.java
@@ -1,12 +1,14 @@
 package com.packt.cardbase.domain;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface OwnerRepository extends CrudRepository<Owner, Long>{
+	Optional<Owner> findByFirstname(String firstName);
 	
 
 }
