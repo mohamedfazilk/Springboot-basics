b89432e886408b7d0418417d9cb3cc3939b8fabb
mohamedfazilk <2114921@cognizant.com>
2022-09-23 15:54:04 +0530

    place the unit testing

--- src/test/java/com/packt/cardbase/CardbaseApplicationTests.java
+++ src/test/java/com/packt/cardbase/CardbaseApplicationTests.java
@@ -1,13 +1,22 @@
 package com.packt.cardbase;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import com.packt.cardbase.web.CartController;
 
 @SpringBootTest
 class CardbaseApplicationTests {
-
+	
+	@Autowired 
+	private CartController carcontroller;
+	
 	@Test
+	@DisplayName("first example test case")
 	void contextLoads() {
+		assertThat(carcontroller).isNotNull();
 	}
 
 }
